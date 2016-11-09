eccfaef7b3b302fea48eddae5c472d914ea61cb8
Stefan Coors <stefan.coors@gmx.net>
2016-11-09 15:54:37 +0100

    small update

--- server_data_summary.R
+++ server_data_summary.R
@@ -1,7 +1,8 @@
 ##### data summary #####
 
 output$summary.datatable = renderDataTable({
-  d = data(); if (is.null(d)) return(NULL)
+  req(data())
+  d = data()
   colnames(d) = make.names(colnames(d)) 
   summarizeColumns(d)
 }, options = list(lengthMenu = c(5, 20, 50), pageLength = 5)
@@ -10,25 +11,39 @@ output$summary.datatable = renderDataTable({
 
 output$summary.vis.hist.var = renderUI({
   d = data()
-  nums <- sapply(d, is.numeric)
-  choices = colnames(d[, nums])
-  # choices = as.list(colnames(data()))
-  selectInput("summary.vis.hist.var", "Choose a variable:", choices = choices, selected = getLast(choices), width = "95%")
+  # nums = sapply(d, is.numeric)
+  # choices = colnames(d[, nums])
+  choices = as.list(colnames(data()))
+  selectInput("summary.vis.var", "Choose a variable:", choices = choices, selected = getLast(choices), width = "95%")
 })
 
 output$summary.vis.hist.nbins = renderUI({
   sliderInput("summary.vis.hist.nbins", "Number of bins", min = 1L, max = 100L, value = 30L, step = 1L, width = "95%")
 })
 
+
+output$summary.vis = renderPlot({
+  req(data())
+  d = data()
+  factors = sapply(d, is.factor)
+  numerics = sapply(d, is.numeric)
+  factor_ch = colnames(d[,factors])
+  num_ch = colnames(d[,numerics])
+  ggplot(data = d, aes(x = as.numeric(d[,input$summary.vis.var]))) + 
+    geom_histogram(aes(y = ..density..), stat = "bin", bins = input$summary.vis.hist.nbins) + 
+    geom_density() + xlab(input$summary.vis.hist.var)
+})  
+
 output$summary.vis.hist = renderPlot({
-  d = data() 
-  if (is.null(d)) return(NULL)
-  ggplot(data = d, aes(x = as.numeric(d[,input$summary.vis.hist.var]))) + 
+  req(data())
+  d = data()
+  ggplot(data = d, aes(x = as.numeric(d[,input$summary.vis.var]))) + 
     geom_histogram(aes(y = ..density..), stat = "bin", bins = input$summary.vis.hist.nbins) + 
     geom_density() + xlab(input$summary.vis.hist.var)
 })    
 
 output$summary.vis.bp.var = renderUI({
+  req(data())
   d = data()
   nums <- sapply(d, is.numeric)
   choices = colnames(d[, nums])
@@ -37,8 +52,8 @@ output$summary.vis.bp.var = renderUI({
 })
 
 output$summary.vis.bp = renderPlot({
-  d = data() 
-  if (is.null(d)) return(NULL)
+  req(data())
+  d = data()
   ggplot(data = d, aes(y = as.numeric(d[,input$summary.vis.bp.var]), x = input$summary.vis.bp.var)) + 
     geom_boxplot() + ylab(input$summary.vis.bp.var) + xlab("")
 }) 
