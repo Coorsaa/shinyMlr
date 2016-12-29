d0bd842c8e019997c2c34781dd8cce6d76111f6d
Stefan Coors <stefan.coors@gmx.net>
2016-12-29 21:41:28 +0100

    cleanup and remove obsolete ui files

--- ui.R
+++ ui.R
@@ -9,11 +9,8 @@ source("ui_tabpanel_summary.R")
 source("ui_tabpanel_preprocessing.R")
 source("ui_tabpanel_task.R")
 source("ui_tabpanel_learners.R")
-source("ui_tabpanel_benchmark.R")
-source("ui_tabpanel_bmrplots.R")
-source("ui_tabpanel_predictionplot.R")
-source("ui_tabpanel_partialdep.R")
 source("ui_tabpanel_train_and_predict.R")
+source("ui_tabpanel_benchmark.R")
 
 shinyUI(
   dashboardPage(
@@ -36,12 +33,7 @@ shinyUI(
         menuItem("Task", tabName = "task", icon = icon("flag")),
         menuItem("Learners", tabName = "learners", icon = icon("cog")),
         menuItem("Train and Predict", tabName = "modelling", icon = icon("graduation-cap")),
-        menuItem("Benchmark", tabName = "benchmark", icon = icon("hourglass-start"))#,
-        #menuItem("Visualisations", tabName = "visualisations", icon = icon("area-chart"),
-        #  menuSubItem("Benchmark Plots", tabName = "bmrplots"),
-        #  menuSubItem("Prediction Plots", tabName = "predplots"),
-        #  menuSubItem("Partial Dep. Plots", tabName = "partdepplots")
-        #)
+        menuItem("Benchmark", tabName = "benchmark", icon = icon("hourglass-start"))
       )
     ),
     
@@ -57,11 +49,8 @@ shinyUI(
         tabItem(tabName = "preprocessing", tabpanel.preprocessing),
         tabItem(tabName = "task", tabpanel.task),
         tabItem(tabName = "learners", tabpanel.learners),
-        tabItem(tabName = "benchmark", tabpanel.benchmark),
-        # tabItem(tabName = "bmrplots", tabpanel.bmrplots),
-        # tabItem(tabName = "predplots", tabpanel.predictionplot),
-        # tabItem(tabName = "partdepplots", tabpanel.partialdep),
-        tabItem(tabName = "modelling", tabpanel.modelling)
+        tabItem(tabName = "modelling", tabpanel.modelling),
+        tabItem(tabName = "benchmark", tabpanel.benchmark)
       )
     )
   )
