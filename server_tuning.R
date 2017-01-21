719ecf243a20f0895bb530424360f20f2499dcc7
Stefan Coors <stefan.coors@gmx.net>
2017-01-21 11:45:28 +0100

    leave out default for tuning par sets

--- server_tuning.R
+++ server_tuning.R
@@ -123,11 +123,11 @@ tuning = eventReactive(input$tune.run, {
       if (param.type == "numeric") {
         param.low = input[[paste0("tune.par.lower.", param)]]
         param.up = input[[paste0("tune.par.upper.", param)]]
-        makeNumericParam(id = param, lower = param.low, upper = param.up, default = param.def)
+        makeNumericParam(id = param, lower = param.low, upper = param.up)#, default = param.def) #FIXME is default necessary somehow?
       } else if (param.type == "integer") {
         param.low = input[[paste0("tune.par.lower.", param)]]
         param.up = input[[paste0("tune.par.upper.", param)]]
-        makeIntegerParam(id = param, lower = param.low, upper = param.up, default = param.def)
+        makeIntegerParam(id = param, lower = param.low, upper = param.up)#, default = param.def)
       } else if (param.type == "discrete") {
         param.box = input[[paste0("tune.par.checkbox", param)]]
         makeDiscreteParam(id = param, values = param.box)
