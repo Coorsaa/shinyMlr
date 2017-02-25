57ac7a835c80427cd93939c673d34e6acb6697ca
Florian Fendt <flo_fendt@gmx.de>
2017-02-25 18:23:17 +0100

    rm commented stuff

--- ui/import.R
+++ ui/import.R
@@ -18,26 +18,3 @@ tabpanel.import = list(
     )
   )
 )
-
-# tabpanel.import = list(fluidRow(
-#   box(width = 12, title = "Import",
-#     htmlOutput("import.text"),
-#     br(),
-#     uiOutput("import.ui"),
-#     hidden(
-#       div(id = "loading.message", align = "center",
-#         h4("Loading datasets from OpenML")
-#       )
-#     )
-#   )),
-#   fluidRow(
-#     box(width = 12,
-#       DT::dataTableOutput("import.preview")
-#     )
-#   ),
-#   fluidRow(uiOutput("tabpanel.browse.openml"))
-# )
-
-
-
-
