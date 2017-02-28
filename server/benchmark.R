ea0ad45805c5d6cf417c91cb707ddab625db7eda
Stefan Coors <stefan.coors@gmx.net>
2017-02-28 12:48:01 +0100

    fixes

--- server/benchmark.R
+++ server/benchmark.R
@@ -118,7 +118,7 @@ output$benchmark.overview = renderDataTable({
   validateLearner(learner$learner)
   validateLearner(lrns = bmr.learners(), check = "err")
   bmr = bmr()
-  # validateExperiment(bmr, "BenchmarkResult")
+  validate(need(class(bmr) == "BenchmarkResult", NULL))
   getBMRAggrPerformances(bmr, as.df = TRUE)
 }, options = list(lengthMenu = c(10, 20), pageLength = 10,scrollX = TRUE))
 
