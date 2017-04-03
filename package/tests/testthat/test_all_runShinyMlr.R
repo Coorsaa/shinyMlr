4f59c2909168a1b0a43a3f82e6e94be6257fe57e
Jakob Richter <code@jakob-r.de>
2017-04-03 14:16:46 +0200

    add a very naive test

--- package/tests/testthat/test_all_runShinyMlr.R
+++ package/tests/testthat/test_all_runShinyMlr.R
@@ -1,10 +1,13 @@
 # FIXME: Add tetsts for runShinyMlr()
 # Possibly use shinytest https://github.com/rstudio/shinytest
 
-# context("runShinyMlr")
+context("runShinyMlr")
 
-# test_that("can connect to app", {
-# })
+test_that("can run app", {
+  expect_message({
+    R.utils::evalWithTimeout(runShinyMlr(port = 445566), timeout = 10, onTimeout = "silent")
+  }, "Listening on http://127.0.0.1:445566")
+})
 
 # test_that("extra args to 'runApp'", {
 # })
